a2055485d9bbf5a3fec944c8c0c18034e6c8e978
Vinicius Silva <vinycd@gmail.com>
2026-01-31 19:18:50 -0300

    Uso de fatia

---
+++ fatia.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Primeira parte da aula
+	arr := [7]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}
+	fatia := arr[0:5]
+	fmt.Println("Fatia:", fatia)
+	//Segunda parte da aula
+	fatia1 := []int{1, 2, 3}
+	fatia2 := append(fatia1, 2, 4, 5)
+	fmt.Println("Fatia1:", fatia1, fatia2)
+	//Terceira parte da aula
+	fatia3 := []int{10, 20, 30, 40, 50}
+	fatia4 := make([]int, 1)
+	copy(fatia4, fatia3)
+	fmt.Println("Fatia3 e Fatia4:", fatia3, fatia4)
+}
